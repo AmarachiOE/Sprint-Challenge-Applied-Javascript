999bfbe04fadd9e46b41d6db6293c8050a3c5818
AmarachiOE <ao.ejiawoko@gmail.com>
2019-03-08 12:19:29 -0500

    completed MVPs. added new articles and my picture to the assets folder to use for my author photo.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -4,7 +4,7 @@ class TabLink {
     this.tabElement = tabElement;
     
     // Get the `data-tab` value from this.tabElement and store it here
-    this.tabData = document.querySelector(`.tab[data-tab = '${this.tabElement.dataset.tab}]`); 
+    this.tabData = this.tabElement.dataset.tab; 
     
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
     
@@ -14,11 +14,11 @@ class TabLink {
       this.cards = document.querySelectorAll('.card');;
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      this.cards = document.querySelector(`.card[data-tab = '${this.tabElement.dataset.tab}']`);
+      this.cards = document.querySelectorAll(`.card[data-tab = '${this.tabData}']`); //${this.tabElement.dataset.tab} stored earlier
     }
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-     // TRY REMOVING ARRAY FROM
+     // remove array.from like tabs component project 3/7?
     this.cards = Array.from(this.cards).map(card => new TabCard(card));
 
     // Add a click event that invokes this.selectTab
