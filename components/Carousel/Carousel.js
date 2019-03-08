626103e2c45b5ae3a063c1618a8d40961638eb45
AmarachiOE <ao.ejiawoko@gmail.com>
2019-03-08 13:49:10 -0500

    STRETCH: attempting to add functionality to carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,24 @@
 class Carousel {
+    constructor(rideElement) {
+        this.rideElement = rideElement;
+        this.leftButton = this.rideElement.querySelector('.left-button');
+        this.rightButton = this.rideElement.querySelector('.right-button');
+        this.images = this.rideElement.querySelectorAll('.carousel img');
+        this.leftButton.addEventListener ('click', () => {
+            this.toggleContent();
+        })
+        this.rightButton.addEventListener ('click', () => {
+            this.toggleContent();
+        })
 
+    }
+
+    toggleContent() {
+        
+    }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel').forEach( ride => new Carousel(ride));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
@@ -12,3 +28,4 @@ let carousel = document.querySelector();
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+
